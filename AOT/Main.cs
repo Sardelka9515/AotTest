e584563018f4f11a56a9d34f53f254d55e9192d4
sardelka9515 <sardelka9515@gmail.com>
2022-12-05 20:30:29 +0800

    Add project files.

---
+++ AOT/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+using static PInvoke.Kernel32;
+namespace AOT
+{
+    public class Main
+    {
+        private const string Dir = @"C:\";
+        public static unsafe uint Init(void* lpParam)
+        {
+            Console.WriteLine("Thread started from DllMain!");
+
+            // Simple filesystem test
+            Console.WriteLine("Directories in "+Dir);
+            foreach (var d in Directory.GetDirectories(Dir))
+            {
+                Console.WriteLine(d);
+            }
+            return 0;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "Hello")]
+        public static void Hello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "DllMain")]
+        public static unsafe bool DllMain(IntPtr hModule, uint ul_reason_for_call, IntPtr lpReserved)
+        {
+            switch (ul_reason_for_call)
+            {
+                case 1:
+                    // Note that we run the init function in another thread so it don't cause a dead-lock
+                    // More info: https://learn.microsoft.com/en-us/windows/win32/dlls/dllmain
+                    CreateThread(null, (UIntPtr)0, Init, null, CreateThreadFlags.None, null);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+}
---
+++ AotTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AotTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        [DllImport("AOT.dll")]
+        public static extern void Hello();
+    }
+}
